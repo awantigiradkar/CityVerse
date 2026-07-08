1bfbbca269b66ef93083061558bd64778cb77b65
awantigiradkar <awantigiradkar1621@gmail.com>
2026-07-08 15:58:33 +0530

    Initializing project folder structure

--- test_config.py
+++
@@ -1,7 +0,0 @@
-# test_config.py
-from src.config import settings
-
-print("Configurations Loaded Successfully!")
-print(f"Database URL: {settings.database_url}")
-print(f"API Port: {settings.API_PORT}")
-print(f"Gemini API Key Loaded: {'Yes' if settings.GEMINI_API_KEY else 'No'}")
